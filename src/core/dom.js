bce0733fd7ed92f374305aa57d3fcfd5ffb031e8
Kirill <nikodakirill386@gmail.com>
2020-07-29 22:58:06 +0300

    finish excel logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,7 +1,8 @@
 class Dom {
   constructor(selector) {
-    // #app
-    this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector
   }
 
   html(html) {
@@ -12,45 +13,97 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
   }
+
   on(eventType, callback) {
     this.$el.addEventListener(eventType, callback)
   }
+
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
   }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
     }
+
     if (Element.prototype.append) {
       this.$el.append(node)
     } else {
       this.$el.appendChild(node)
     }
+
     return this
   }
+
   get data() {
     return this.$el.dataset
   }
+
   closest(selector) {
     return $(this.$el.closest(selector))
   }
+
   getCoords() {
     return this.$el.getBoundingClientRect()
   }
+
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
   }
+
   css(styles = {}) {
-    Object.keys(styles).forEach((key) => this.$el.style[key] = styles[key])
+    Object
+        .keys(styles)
+        .forEach(key => {
+          this.$el.style[key] = styles[key]
+        })
+  }
+
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
   }
 }
 
-// event.target
 export function $(selector) {
   return new Dom(selector)
 }
